16dfacdd3dccc4cc3f6846959d0f86c7c8401d4c
Ryan Sasson <ryan.sasson@gmail.com>
2015-06-06 21:27:41 -0700

    Added simple info window

--- js/main.js
+++ js/main.js
@@ -13,6 +13,31 @@ var neighborhoods = [
   new google.maps.LatLng(37.77973776283843, -122.39009857177734)
 ];
 
+var contentString = '<div id="content">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+    '<div id="bodyContent">'+
+    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+    'sandstone rock formation in the southern part of the '+
+    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+    'south west of the nearest large town, Alice Springs; 450&#160;km '+
+    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+    'Aboriginal people of the area. It has many springs, waterholes, '+
+    'rock caves and ancient paintings. Uluru is listed as a World '+
+    'Heritage Site.</p>'+
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+    '(last visited June 22, 2009).</p>'+
+    '</div>'+
+    '</div>';
+
+var infowindow = new google.maps.InfoWindow({
+    content: contentString
+});
+
 function initialize() {
   var missionBay = new google.maps.LatLng(37.767997, -122.3921315)
   var mapOptions = {
@@ -32,12 +57,17 @@ function drop() {
 
 // Add a marker to the map and push to the array.
 function addMarkerWithTimeout(position, timeout) {
+  var marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    animation: google.maps.Animation.DROP
+    // TODO add an on click style change (change color or something)
+  });
+  google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+  });
   window.setTimeout(function() {
-    markers.push(new google.maps.Marker({
-      position: position,
-      map: map,
-      animation: google.maps.Animation.DROP
-    }));
+    markers.push(marker);
   }, timeout);
 }
 
